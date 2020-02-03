f561df564402d348af8a0a142d4acb653853010b
hieu01412 <hieu01412@gmail.com>
2020-02-03 17:50:44 +0700

    123

---
+++ Setup/InstallData.php
@@ -0,0 +1,24 @@
+<?php
+use Magento\Framework\Setup\InstallDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+
+/**
+ * @codeCoverageIgnore
+ */
+class InstallData implements InstallDataInterface
+{
+    /**
+     * {@inheritdoc}
+     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
+     */
+    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $setup->startSetup();
+
+        // Install here your data...
+
+
+        $setup->endSetup();
+    }
+}
